5597057e459bf1de33b3aa8cc9a2fd39872462ec
Tiara Hudyana <tiara@healthpalsinc.com>
2018-03-19 20:51:15 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('form').on('submit', function(event){
+  event.preventDefault();
+  var item = '<li>' + $('#enter').val() + '</li>'
+  $('uo').append();
+  $('ul li').on('click', function(){
+    $(this.css('text-decoration', 'line-through');
+  });
+  $('#enter').val('';)
+
+};
